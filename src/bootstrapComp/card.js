9193227862b3592da2ab8923eb31e8eca9bc6de7
fha96 <fahadalrimawe96@hotmail.com>
2022-07-25 19:03:10 +0300

    add some features and used state

---
+++ src/bootstrapComp/card.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import Card from 'react-bootstrap/Card';
+// import '../App.css';
+
+
+// class NewCard extends React.Component {
+//   render(){
+//     return (
+//     <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src={this.props.link} />
+//       <Card.Body>
+//         <Card.Title>{this.props.title}</Card.Title>
+//         <Card.Text>
+//             {this.props.decripe}
+//         </Card.Text>
+//       </Card.Body>
+//       <span></span>
+//     </Card>
+//   );
+// }
+// }
+
+// export default NewCard;
--- src/header.js
+++ src/header.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Navigation from "./bootstrapComp/navigation";
+
 
 class Header extends React.Component {
     render(){
 
         return (
+            <div>
             <h1 className="Header">Welcome to my first App using React</h1>
+            <Navigation />
+            </div>
         );
     }
 }
