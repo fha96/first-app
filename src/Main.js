6ac8ff3a88655f1744ef7bf937be966c6c83ed8e
fha96 <fahadalrimawe96@hotmail.com>
2022-07-26 14:21:48 +0300

    complete the task with more feactures unsing bootstrap

--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,24 @@
 import React from "react";
 import HornedBeast from "./hornedbeast";
 import Data from './data.json';
-import NewCard from "./bootstrapComp/card";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import './App.css';
 import { Card } from "react-bootstrap";
 
 class Main extends React.Component{
     render(){
         return(
-            Data.map( data =>{
-                return(
-                    <div className="main-container" key={data.id}>
+            <Row xs={1} md={3} className="g-4">
+           { Data.map( data =>{
+             return(    <Col>
                         <HornedBeast link={data.image_url} title={data.title} discripe={data.description}/>
-                        {/* <NewCard title={data.title} link={data.image_url} decripe={data.description} /> */}
-                        
-                    </div>
-                )
-            })
-           
+                 </Col>
+                 )  
+            })}
+            </Row>
         )
+        
     }
 }
 export default Main; 
