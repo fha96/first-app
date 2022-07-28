e4bee4e775d419719e41169a062974fa3cebf065
fha96 <fahadalrimawe96@hotmail.com>
2022-07-28 16:29:44 +0300

    reformat code for lab3

--- src/App.js
+++ src/App.js
@@ -3,18 +3,46 @@ import Header from './header';
 import Footer from './footer';
 import Main from './Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './data.json';
 import React from 'react';
+import SelectedBeast from './bootstrapComp/SelectedBeast';
 
 
-function App(){
+class App extends React.Component{
 
+  constructor(props){
+    super(props);
+
+    this.state={
+      allBeast: data,
+      beastSelect:{},
+      displayModal:false
+    }
+
+  }
+  showModal=(beast)=>{
+    this.setState({
+      displayModal:true,
+      beastSelect:beast
+    })
+
+  }
+handleClose=()=>{
+  this.setState({
+    displayModal:false
+  })
+}
+
+render(){
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main data={this.state.allBeast} showModal={this.showModal}/>
+      <SelectedBeast show={this.state.displayModal}  handleClose={this.handleClose} beastS={this.state.beastSelect} />
       <Footer />
     </div>
   );
+}
 }
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,21 @@
 import React from "react";
 import HornedBeast from "./hornedbeast";
-import Data from './data.json';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import './App.css';
-import { Card } from "react-bootstrap";
+
 
 class Main extends React.Component{
     render(){
         return(
             <Row xs={1} md={3} className="g-4">
-           { Data.map( data =>{
-             return(    <Col>
-                        <HornedBeast link={data.image_url} title={data.title} discripe={data.description}/>
+           { this.props.data && this.props.data.map( (item,idx) =>{
+             return(<Col key={idx}>
+                        <HornedBeast showModal={this.props.showModal} link={item.image_url} title={item.title} discripe={item.description} data={item} />
                  </Col>
                  )  
             })}
-            </Row>
+            </Row>  
         )
         
     }
